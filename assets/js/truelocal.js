9f256d6e9e8c24f153d11a89278796eeac5dc50b
Richard <40692203+richard-hutchings@users.noreply.github.com>
2020-11-15 18:51:25 +1100

    Initial Upload

---
+++ assets/js/truelocal.js
@@ -0,0 +1,6 @@
+(function(t,r,i,p){i=t.createElement(r);
+    i.src="https://webcomponents.localsearch.com.au/widgets/locallyRecommended?embed=true&id=10024707&location=robina,qld&target=locallyRecommended";
+    i.async=true;p=t.getElementsByTagName(r)[0];
+    p.parentNode.insertBefore(i,p)})(document,"script");
+
+//immediate="true"
